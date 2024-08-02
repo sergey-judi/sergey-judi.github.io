56872fb55da1aef1792527c5d8299111d0c01fee
sergey-judi <serega7qwerty@gmail.com>
2024-08-02 20:06:12 +0300

    feat: added education section

---
+++ src/constant/education-data.js
@@ -0,0 +1,37 @@
+import {
+    DiamondLgBlue,
+    DiamondLgGreen,
+    DiamondLgPink
+  } from "../assets/image";
+
+const EducationData = [
+    {
+        id: "edu-1",
+        icon: DiamondLgBlue,
+        course: "Bachelor's degree, Computer Science",
+        institution: "National Technical University of Ukraine 'Kyiv Polytechnic Institute', IASA​",
+        degree: "Diploma",
+        startDate: "September 2018",
+        endDate: "June 2022"
+    },
+    {
+        id: "edu-2",
+        icon: DiamondLgPink,
+        course: "Master's degree, Computer Science",
+        institution: "National Technical University of Ukraine 'Kyiv Polytechnic Institute', IASA​",
+        degree: "Diploma",
+        startDate: "September 2022",
+        endDate: "January 2024"
+    },
+    {
+        id: "edu-3",
+        icon: DiamondLgGreen,
+        course: "Java & DevOps courses",
+        institution: "Parimatch Tech Academy",
+        degree: "Certificate",
+        startDate: "May 2021",
+        endDate: "September 2021"
+    }
+];
+
+export default EducationData;
